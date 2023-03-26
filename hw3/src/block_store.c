35ddd3a91bc434c3f68a07120bb29a2c6032fab6
GraniteBock <graniteb@ksu.edu>
2023-03-26 17:18:10 -0500

    reset

--- hw3/src/block_store.c
+++ hw3/src/block_store.c
@@ -17,7 +17,7 @@
 
 // include more if you need
 
-// You might find this handy.  I put it around unused parameters, but you should
+// You might find this handy.  Is put it around unused parameters, but you should
 // remove it before you submit. Just allows things to compile initially.
 #define UNUSED(x) (void)(x)
 
@@ -30,13 +30,24 @@ typedef struct block_store
 
 block_store_t *block_store_create()
 {
-    block_store_t *bs_pointer = malloc(sizeof(block_store_t)); // calloc(0,sizeof(pow(256,9)));
+    block_store_t *bs_pointer = malloc(sizeof(block_store_t)); //calloc(0,sizeof(pow(256,9)));
     bs_pointer->bitmap = bitmap_create(BLOCK_STORE_NUM_BLOCKS);
-    // need to put bitmap into data
+    //need to put bitmap into data
     bitmap_set(bs_pointer->bitmap, 127);
+    //bitmap_set(bs_pointer->bitmap, 128);
     return bs_pointer;
 }
 
+void block_store_destroy(block_store_t *const bs)
+{
+
+    if(bs == NULL)
+    {
+        return;
+    }
+
+    free(bs);
+}
 void block_store_destroy(block_store_t *const bs)
 {
 
@@ -73,60 +84,61 @@ size_t block_store_allocate(block_store_t *const bs)
 
 bool block_store_request(block_store_t *const bs, const size_t block_id)
 {
-    if (bs == NULL)
+    if(bs == NULL)
     {
         return false;
     }
-    if (block_id >= bitmap_get_bits(bs->bitmap))
+    if(block_id >= bitmap_get_bits(bs->bitmap))
     {
         return false;
     }
-    if (!(bitmap_test(bs->bitmap, block_id)))
+    if(!(bitmap_test(bs->bitmap,block_id))) 
     {
         bitmap_set(bs->bitmap, block_id);
         return true;
-    }
+    } 
     return false;
 }
 
 void block_store_release(block_store_t *const bs, const size_t block_id)
 {
-    if (bs != NULL)
+    if(bs != NULL )
     {
-        if (block_id >= bitmap_get_bits(bs->bitmap))
+        if(block_id >= bitmap_get_bits(bs->bitmap))
         {
-            bitmap_reset(bs->bitmap, block_id);
+            bitmap_reset(bs->bitmap,block_id); 
         }
     }
+    
 }
 
 size_t block_store_get_used_blocks(const block_store_t *const bs)
 {
-    if (bs == NULL)
+    if(bs == NULL)
     {
         return SIZE_MAX;
     }
     else
     {
-        // return bs->used;
+        //return bs->used; not working yet
         return bitmap_total_set(bs->bitmap) - 1;
     }
     /* for(int i = 0; i < 256; i++)     //iterates through blocks
     {
-        if(bitmap_test(bs->bitmap,i))
+        if(bitmap_test(bs->bitmap,i))   
         {
             count++;                    //add to counter if set
         }
 
     } */
 
-    // UNUSED(bs);
-    // return count;                       //return count
+    //UNUSED(bs);
+    //return count;                       //return count
 }
 
 size_t block_store_get_free_blocks(const block_store_t *const bs)
 {
-    if (bs == NULL)
+    if(bs == NULL)
     {
         return SIZE_MAX;
     }
@@ -135,23 +147,33 @@ size_t block_store_get_free_blocks(const block_store_t *const bs)
 
 size_t block_store_get_total_blocks()
 {
-    return 255;
+    return BLOCK_STORE_AVAIL_BLOCKS;
 }
 
 size_t block_store_read(const block_store_t *const bs, const size_t block_id, void *buffer)
 {
-    UNUSED(bs);
-    UNUSED(block_id);
-    UNUSED(buffer);
-    return 0;
+    if(bs == NULL || buffer == NULL)
+    {
+        return 0;
+    }
+    buffer = malloc(BLOCK_SIZE_BYTES);
+    memcpy(buffer,&(bs->data)[block_id],BLOCK_SIZE_BYTES);
+    return BLOCK_SIZE_BYTES;
 }
 
 size_t block_store_write(block_store_t *const bs, const size_t block_id, const void *buffer)
 {
-    UNUSED(bs);
+    if(bs == NULL || buffer == NULL||block_id >=BLOCK_STORE_NUM_BLOCKS)
+    {
+        return 0;
+    }
+    if(bs->data == NULL)
+    {
+        return 0;
+    }
+    //memcpy(&(bs->data[block_id]), buffer,BLOCK_SIZE_BYTES);
     UNUSED(block_id);
-    UNUSED(buffer);
-    return 0;
+    return BLOCK_SIZE_BYTES;
 }
 
 block_store_t *block_store_deserialize(const char *const filename)
